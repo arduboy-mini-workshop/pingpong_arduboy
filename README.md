72c7c61575fedf25d2be789e28a75db18757438f
Dang Quoc Huy <huysamdua@yahoo.com>
2018-03-24 19:28:17 +0700

    init proj

---
+++ README.md
@@ -0,0 +1,41 @@
+# Ping Pong game
+
+**Ping Pong** game for Arduboy
+
+Credit to [crait](http://www.twitter.com/crait), I'm just following his awesome tutorial [here](https://community.arduboy.com/t/make-your-own-arduboy-game-part-7-make-pong-from-scratch/2615) and make some text changes :-) But it's fun.
+
+## In this project, I learnt
+
+- [x] Set up the new sketch
+- [x] Create a title screen, game screen, win screen, and lose screen
+- [x] Create a ball that bounces around the screen
+- [x] Create and control a paddle
+- [x] Create the computer’s paddle
+- [x] Programming collision
+- [x] Adjusting the AI
+- [x] Scoring
+- [x] Creating a new ball and resetting the game
+
+## Screenshot
+
+![alt screenshot](screenshot.jpg)
+
+## Video Walkthrough
+
+[![Video Walkthrough](video_cover.png)](https://youtu.be/ts8J-bWwhEw)
+
+## License
+
+    Copyright [2018] [Huy Dang]
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
---
+++ pingpong_arduboy.ino
@@ -0,0 +1,187 @@
+//Jonathan Holmes (crait)
+//December 7th, 2016
+//A simple Pong clone
+
+#include <Arduboy.h>
+Arduboy arduboy;
+
+//Variables declared here
+int gamestate = 0;
+int justpressed = 0;
+int ballx = 62;
+int bally = 0;
+int ballsize = 4;
+int ballright = 1;
+int balldown = 1;
+int paddlewidth = 4;
+int paddleheight = 9;
+int playerx = 0;
+int playery = 0;
+int computery = 0;
+int computerx = 127 - paddlewidth;
+int playerscore = 0;
+int computerscore = 0;
+
+void setup() {
+	arduboy.begin();
+	//Seed the random number generator
+	srand(7/8);
+	//Set the game to 60 frames per second
+	arduboy.setFrameRate(60);
+	arduboy.clear();
+}
+
+void loop() {
+	//Prevent the Arduboy from running too fast
+	if(!arduboy.nextFrame()) {
+		return;
+	}
+	arduboy.clear();
+	//Game code here
+	switch( gamestate ) {
+		case 0:
+			//Title screen
+			arduboy.setCursor(0, 0);
+			arduboy.print("PING PONG Game");
+			arduboy.setCursor(0, 12);
+			arduboy.print("Credit: crait");
+			arduboy.setCursor(0, 36);
+			arduboy.print("Copy & Paste:");
+			arduboy.setCursor(0, 48);
+			arduboy.print("HUY DANG ^__^");
+			//Change the gamestate
+			if(arduboy.pressed(A_BUTTON) and justpressed == 0) {
+				justpressed = 1;
+				gamestate = 1;
+			}
+			break;
+		case 1:
+			//Gameplay screen
+      //Display the player's score
+      arduboy.setCursor(5, 0);
+      arduboy.print("You:");
+			arduboy.setCursor(30, 0);
+			arduboy.print(playerscore);
+			//Display the computer's score
+			arduboy.setCursor(115, 0);
+			arduboy.print(computerscore);
+			//Draw the ball
+			arduboy.fillRect(ballx, bally, ballsize, ballsize, WHITE);
+			//Move the ball right
+			if(ballright == 1) {
+				ballx = ballx + 1;
+			}
+			//Move the ball left
+			if(ballright == -1) {
+				ballx = ballx - 1;
+			}
+			//Move the ball down
+			if(balldown == 1) {
+				bally = bally + 1;
+			}
+			//Move the ball up
+			if(balldown == -1) {
+				bally = bally - 1;
+			}
+			//Reflect the ball off of the top of the screen
+			if(bally == 0) {
+				balldown = 1;
+			}
+			//Reflect the ball off of the bottom of the screen
+			if(bally + ballsize == 63) {
+	      balldown = -1;
+      }
+      //Draw the player's paddle
+      arduboy.fillRect(playerx, playery, paddlewidth, paddleheight, WHITE);
+      //If the player presses Up and the paddle is not touching the top of the screen, move the paddle up
+			if(arduboy.pressed(UP_BUTTON) and playery > 0) {
+				playery = playery - 1;
+			}
+			//If the player presses down and the paddle is not touching the bottom of the screen, move the paddle down
+			if(arduboy.pressed(DOWN_BUTTON) and playery + paddleheight < 63) {
+				playery = playery + 1;
+			}
+			//Draw the computer's paddle
+			arduboy.fillRect(computerx, computery, paddlewidth, paddleheight, WHITE);
+			//If the ball is higher than the computer's paddle, move the computer's paddle up
+			if(ballx > 115 or rand() % 20 == 1) {
+      	if(bally < computery) {
+      		computery = computery - 1;
+      	}
+      	if(bally + ballsize > computery + paddleheight) {
+      		computery = computery + 1;
+      	}
+      }
+      
+      
+      //If the ball moves off of the screen to the left...
+			if(ballx < -10) {
+				//Move the ball back to the middle of the screen
+				ballx = 63;
+				//Give the computer a point
+				computerscore = computerscore + 1;
+			}
+			//If the ball moves off of the screen to the right....
+			if(ballx > 130) {
+				//Move the ball back to the middle of the screen
+				ballx = 63;
+				//Give the player a point
+				playerscore = playerscore + 1;
+			}
+			//Check if the player wins
+			if(playerscore == 5) {
+				gamestate = 2;
+			}
+			//Check if the computer wins
+			if(computerscore == 5) {
+				gamestate = 3;
+			}
+      //If the ball makes contact with the player's paddle, bounce it back to the right
+			if(ballx == playerx + paddlewidth and playery < bally + ballsize and playery + paddleheight > bally) {
+				ballright = 1;
+			}
+			//If the ball makes contact with the computer's paddle, bounce it back to the left
+			if(ballx + ballsize == computerx and computery < bally + ballsize and computery + paddleheight > bally) {
+				ballright = -1;
+			}
+			//Change the gamestate
+			if(arduboy.pressed(A_BUTTON) and justpressed == 0) {
+				justpressed = 1;
+				gamestate = 2;
+			}
+			break;
+		case 2:
+			//Win screen
+			arduboy.setCursor(0, 0);
+			arduboy.print("Win Screen");
+			//Change the gamestate
+			if(arduboy.pressed(A_BUTTON) and justpressed == 0) {
+				justpressed = 1;
+				resetgame();
+				gamestate = 0;
+			}
+			break;
+		case 3:
+			//Game over screen
+			arduboy.setCursor(0, 0);
+			arduboy.print("Game Over Screen");
+			//Change the gamestate
+			if(arduboy.pressed(A_BUTTON) and justpressed == 0) {
+				justpressed = 1;
+				resetgame();
+				gamestate = 0;
+			}
+			break;
+	}
+	//Check if the button is being held down
+	if(arduboy.notPressed(A_BUTTON)) {
+		justpressed = 0;
+	}
+	arduboy.display();
+}
+
+void resetgame() {
+	ballx = 63;
+	playerscore = 0;
+	computerscore = 0;
+}
